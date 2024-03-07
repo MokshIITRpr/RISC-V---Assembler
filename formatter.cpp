9f45fe0047158b8f59dff63800214fe50d82137c
MokshIITRpr <149925242+MokshIITRpr@users.noreply.github.com>
2024-03-07 23:32:04 +0530

    Update formatter.cpp

--- formatter.cpp
+++ formatter.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
+#include <fstream>
 using namespace std;
 #define ll long long
-#include <fstream>
+#define Start 0x10000000
+ll size = 0;
 // I format -- > imm : rs1 : func3 : rd : opcode
 // U format -- >
 void getOpcode(string inp, string s)
 {
-    //hello
-    // add the opcode for final output string of instruction
+    // hello
+    //  add the opcode for final output string of instruction
     if (inp == "addi" || inp == "andi" || inp == "ori")
         s += "0010011";
     else if (inp == "lb" || inp == "ld" || inp == "lw" || inp == "lh")
@@ -31,8 +33,12 @@ void getOpcode(string inp, string s)
 
 void getRegister(string inp, string s)
 {
-    if (s == "error")
+    if (s == "error" || inp[0] != 'x')
+    {
+        s = "error";
         return;
+    }
+
     string neo;
     for (ll i = 1; i < inp.size(); i++)
         neo += inp[i];
@@ -53,6 +59,11 @@ void getRegister(string inp, string s)
         return;
     }
     ll get = stoi(neo);
+    if (get >= 32 || get < 0)
+    {
+        s = "error";
+        return;
+    }
     for (ll i = 0; i < 5; i++)
     {
         if (get & (1 << i))
@@ -79,38 +90,40 @@ void getFunc7(string inp, string s)
         s = "error";
 }
 
-void getFunc3(string inp, string s) {
-    switch (inp[0]) {
-        case 'a':
-            s += (inp == "add" || inp == "addi") ? "000" : "001";
-            break;
-        case 'b':
-            s += (inp == "beq") ? "000" : ((inp == "bne") ? "001" : ((inp == "blt") ? "100" : ((inp == "bge") ? "101" : "")));
-            break;
-        case 'd':
-            s += ( inp == "ld" || inp == "sd") ? "011" : ((inp == "div")?"100":"");
-            break;
-        case 'l':
-            s += (inp == "lb") ? "000" : ((inp == "lh") ? "001" : ((inp == "lw")?"010":""));
-            break;
-        case 'm':
-            s += (inp == "mul") ? "000" : "";
-            break;
-        case 'o':
-            s += (inp == "or" || inp == "ori") ? "110" : "";
-            break;
-        case 'r':
-            s += (inp == "sra" || inp == "srl") ? "101" : ((inp == "rem")?"110":"");
-            break;
-        case 's':
-            s += (inp == "sb") ? "000" : ((inp == "sh")?"001":((inp == "sw") ? "010" : ((inp == "sll") ? "001" : ((inp == "slt") ? "010" : ((inp == "sub") ? "000" : "")))));
-            break;
-        case 'x':
-            s += (inp == "xor") ? "100" : "";
-            break;
-        default:
-            s += "111";
-            break;
+void getFunc3(string inp, string s)
+{
+    switch (inp[0])
+    {
+    case 'a':
+        s += (inp == "add" || inp == "addi") ? "000" : "001";
+        break;
+    case 'b':
+        s += (inp == "beq") ? "000" : ((inp == "bne") ? "001" : ((inp == "blt") ? "100" : ((inp == "bge") ? "101" : "")));
+        break;
+    case 'd':
+        s += (inp == "ld" || inp == "sd") ? "011" : ((inp == "div") ? "100" : "");
+        break;
+    case 'l':
+        s += (inp == "lb") ? "000" : ((inp == "lh") ? "001" : ((inp == "lw") ? "010" : ""));
+        break;
+    case 'm':
+        s += (inp == "mul") ? "000" : "";
+        break;
+    case 'o':
+        s += (inp == "or" || inp == "ori") ? "110" : "";
+        break;
+    case 'r':
+        s += (inp == "sra" || inp == "srl") ? "101" : ((inp == "rem") ? "110" : "");
+        break;
+    case 's':
+        s += (inp == "sb") ? "000" : ((inp == "sh") ? "001" : ((inp == "sw") ? "010" : ((inp == "sll") ? "001" : ((inp == "slt") ? "010" : ((inp == "sub") ? "000" : "")))));
+        break;
+    case 'x':
+        s += (inp == "xor") ? "100" : "";
+        break;
+    default:
+        s += "111";
+        break;
     }
 }
 
@@ -152,15 +165,153 @@ void getImmediate(string inp, string s)
     }
 }
 
+// add data to memory
+ll getData(string line)
+{
+    // supports ---> binary + hex + ascii + integer
+    string final;
+    ll ret = 0;
+    if (line[0] == '0' && line[1] == 'x')
+    {
+        for (ll i = 2; i < min((ll)line.size(), 10ll); i++)
+            final += line[i];
+        ret = stoi(final, 0, 16);
+    }
+    else if (line[0] == '0')
+    {
+        for (ll i = 1; i < line.size(); i++)
+            final += line[i];
+        ret = stoi(final, 0, 2);
+    }
+    else if ((ll)(line[0] - '0') < 0 || (ll)(line[0] - '0') > 9)
+    { // no intger detected
+        for (ll i = 0; i < line.size(); i++)
+            ret += (ll)line[i];
+    }
+    else
+        ret = stoi(line);
+    return ret;
+}
+
+void addMemory(map<string, ll> &label, string txt, ll mem_arr[200])
+{
+
+    istringstream iss(txt);
+    string line;
+    // first is label
+    iss >> line;
+    string temp = line;
+    if (line[line.size() - 1] == ':')
+    {
+        temp = "";
+        for (ll i = 0; i < line.size() - 1; i++)
+            temp += line[i];
+        line = temp;
+    }
+    label[temp] = Start + size; // marking for la
+    if (line[line.size() - 1] != ':')
+        iss >> line; // jump to next line
+    iss >> line;
+    if (line == ".byte")
+    {
+        while (iss >> line)
+        {
+            if (line == ",")
+                continue;
+            ll get = getData(line);
+            mem_arr[size] = get;
+            size++;
+        }
+    }
+    else if (line == ".word")
+    {
+        while (iss >> line)
+        {
+            if (line == ",")
+                continue;
+            ll get = getData(line);
+
+            // now we have data in integer format and need to break datat into 4 parts
+            ll mask = 255; // extraxt 8 bits
+            for (ll i = 0; i < 4; i++)
+            {
+                ll g = mask & get;
+                get >>= 8;
+                mem_arr[size] = g;
+                size++;
+                // we have added to one byte
+            }
+        }
+    }
+    else if (line == ".asciiz")
+    {
+        while (iss >> line)
+        {
+            if (line == ",")
+                continue;
+            ll get = 0;
+            for (ll i = 0; i < line.size(); i++)
+                get += (ll)line[i];
+            mem_arr[size] = get;
+            size++;
+        }
+    }
+    return;
+}
+// dsiplay the memeory for user purposes
+void display_memory(ll mem_arr[])
+{
+    for (ll i = 0; i < 200; i += 4)
+    {
+        for (ll j = i + 3; j >= i; j--)
+        {
+            ll val = mem_arr[j];
+            string result = "00";
+            ll add = val % 16;
+            if (add >= 10)
+                result[1] = 'A' + add - 10;
+            else
+                result[1] = '0' + add;
+            val /= 16;
+            add = val % 16;
+            if (add >= 10)
+                result[0] = 'A' + add - 10;
+            else
+                result[0] = '0' + add;
+            cout << result << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     ofstream op("output.txt");
-    ifstream ip("input.txt");
+    ifstream file("input.txt");
     string txt;
-    while (getline (ip, txt)) {
-        
+    ll mem_arr[200];
+    for (ll i = 0; i < 200; i++)
+        mem_arr[i] = 0;
+    map<string, ll> label;
+    ll flag = 0;
+    ll memory = Start; // memory
+
+    ll pc = 0; // program counter
+    getline(file, txt);
+
+    if (txt == ".data")
+    {
+        while (getline(file, txt))
+        {
+
+            if (txt == ".text")
+                break;
+            addMemory(label, txt, mem_arr);
+        }
     }
+    display_memory(mem_arr);
+
     op.close();
-    ip.close();
+    file.close();
     return 0;
 }
