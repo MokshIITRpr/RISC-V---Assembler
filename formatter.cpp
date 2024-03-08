c39fbf65d2ca1aae0fcbab9435adfeca3bb2bedd
RajIITRpr <149925546+RajYadavIITRpr@users.noreply.github.com>
2024-03-08 23:08:49 +0530

    Update formatter.cpp

--- formatter.cpp
+++ formatter.cpp
@@ -6,6 +6,87 @@ using namespace std;
 ll size = 0;
 // I format -- > imm : rs1 : func3 : rd : opcode
 // U format -- >
+unordered_map<string, string> mpp;
+
+void initializeMap() {
+    pair<string,string>old_arr[] = 
+   {
+    make_pair("x0", "x0"),
+    make_pair("x1", "x1"), 
+    make_pair("x2", "x2"), 
+    make_pair("x3", "x3"), 
+    make_pair("x4", "x4"), 
+    make_pair("x5", "x5"), 
+    make_pair("x6", "x6"), 
+    make_pair("x7", "x7"), 
+    make_pair("x8", "x8"), 
+    make_pair("x9", "x9"), 
+    make_pair("x10", "x10"),
+    make_pair("x11", "x11"), 
+    make_pair("x12", "x12"), 
+    make_pair("x13", "x13"), 
+    make_pair("x14", "x14"), 
+    make_pair("x15", "x15"), 
+    make_pair("x16", "x16"), 
+    make_pair("x17", "x17"), 
+    make_pair("x18", "x18"), 
+    make_pair("x19", "x19"), 
+    make_pair("x20", "x20"),
+    make_pair("x21", "x21"), 
+    make_pair("x22", "x22"), 
+    make_pair("x23", "x23"), 
+    make_pair("x24", "x24"), 
+    make_pair("x25", "x25"), 
+    make_pair("x26", "x26"), 
+    make_pair("x27", "x27"), 
+    make_pair("x28", "x28"), 
+    make_pair("x29", "x29"), 
+    make_pair("x30", "x30"),
+    make_pair("x31", "x31"), 
+    make_pair("zero", "x0"), 
+    make_pair("ra", "x1"), 
+    make_pair("sp", "x2"), 
+    make_pair("gp", "x3"), 
+    make_pair("tp", "x4"), 
+    make_pair("t0", "x5"), 
+    make_pair("t1", "x6"), 
+    make_pair("t2", "x7"), 
+    make_pair("s0", "x8"), 
+    make_pair("s1", "x9"), 
+    make_pair("a0", "x10"),
+    make_pair("a1", "x11"), 
+    make_pair("a2", "x12"), 
+    make_pair("a3", "x13"), 
+    make_pair("a4", "x14"), 
+    make_pair("a5", "x15"), 
+    make_pair("a6", "x16"), 
+    make_pair("a7", "x17"), 
+    make_pair("s2", "x18"), 
+    make_pair("s3", "x19"), 
+    make_pair("s4", "x20"),
+    make_pair("s5", "x21"), 
+    make_pair("s6", "x22"), 
+    make_pair("s7", "x23"), 
+    make_pair("s8", "x24"), 
+    make_pair("s9", "x25"), 
+    make_pair("s10", "x26"), 
+    make_pair("s11", "x27"), 
+    make_pair("t3", "x28"), 
+    make_pair("t4", "x29"), 
+    make_pair("t5", "x30"),
+    make_pair("t6", "x31"),
+    
+   };
+
+   int mppsize = (sizeof(old_arr) / 
+            sizeof(old_arr[0]));
+   
+   unordered_map<string, string> mpp1(old_arr, 
+                              old_arr + mppsize);
+    mpp=mpp1;
+    
+}
+
 string getOpcode(string inp, string s)
 {
     // hello
@@ -413,6 +494,7 @@ void assemble(string txt)
 
 int main()
 {
+    initializeMap();
     ofstream op("output.txt");
     ifstream file("input.txt");
     string txt;
