1f574977715ff42287777b303e23d73fa9c0ee52
MokshIITRpr <149925242+MokshIITRpr@users.noreply.github.com>
2024-03-09 15:23:19 +0530

    Update cs204func.cpp

--- cs204func.cpp
+++ cs204func.cpp
@@ -25,6 +25,11 @@ void getRformat(string txt)
     for (ll i = 5; i >= 0; i--)
     {
         final += ret[i];
+        if (ret[i] == "error")
+        {
+            store.push_back("error");
+            return;
+        }
     }
     store.push_back(final);
 }
@@ -55,6 +60,11 @@ void getIformat(string txt)
     for (ll i = 4; i >= 0; i--)
     {
         final += ret[i];
+        if (ret[i] == "error")
+        {
+            store.push_back("error");
+            return;
+        }
     }
     store.push_back(final);
 }
@@ -105,6 +115,11 @@ void getSformat(string txt)
     for (ll i = 5; i >= 0; i--)
     {
         final += ret[i];
+        if (ret[i] == "error")
+        {
+            store.push_back("error");
+            return;
+        }
     }
     store.push_back(final);
 }
@@ -128,11 +143,14 @@ void getSBformat(string txt)
     if (line == ",")
         iss >> line;
     string final;
+
     if (label.find(line) == label.end())
     {
-        final = "No such label was found\n";
+        store.push_back("error");
+        return;
     }
     ll imm = label[line] - pc;
+    // if(imm<0)imm+=
     string get = to_string(imm);
     ret[5] = getImmediateSB(get, ret[5]);
     string temp = ret[5];
@@ -144,14 +162,91 @@ void getSBformat(string txt)
         ret[1] += temp[i];
     ret[1] += temp[1];
     for (ll i = 5; i >= 0; i--)
+    {
         final += ret[i];
+        if (ret[i] == "error")
+        {
+            store.push_back("error");
+            return;
+        }
+    }
     store.push_back(final);
 }
 
 void getUformat(string txt)
 {
+    istringstream iss(txt); // create string stream
+    string line;
+    iss >> line;
+    string keep = line;
+    string final;
+    vector<string> ret(3, "");
+    ret[0] = getOpcode(line, ret[0]);
+    iss >> line;
+    if (line == ",")
+        iss >> line;
+    ret[1] = getRegister(line, ret[1]);
+    iss >> line;
+    if (line == ",")
+        iss >> line;
+    ret[2] = getImmediateU(line, ret[2]);
+    for (ll i = 2; i >= 0; i--)
+    {
+        final += ret[i];
+        if (ret[i] == "error")
+        {
+            store.push_back("error");
+            return;
+        }
+    }
+    store.push_back(final);
 }
 
 void getUJformat(string txt)
 {
+    istringstream iss(txt); // create string stream
+    string line;
+    iss >> line;
+    string keep = line;
+    string final;
+    vector<string> ret(3, "");
+    ret[0] = getOpcode(line, ret[0]);
+    iss >> line;
+    if (line == ",")
+        iss >> line;
+    ret[1] = getRegister(line, ret[1]);
+    iss >> line;
+    if (line == ",")
+        iss >> line;
+    if (label.find(line) == label.end())
+    {
+        store.push_back("error");
+        return;
+    }
+    ll jump = label[line] - pc;
+    for (ll i = 20; i >= 1; i--)
+    {
+        if ((1ll << i) & jump)
+            ret[2] += '1';
+        else
+            ret[2] += '0';
+    }
+    string temp = ret[2];
+    ret[2] = "";
+    ret[2] += temp[0];
+    for (ll i = 10; i < 20; i++)
+        ret[2] += temp[i];
+    ret[2] += temp[9];
+    for (ll i = 1; i <= 8; i++)
+        ret[2] += temp[i];
+    for (ll i = 2; i >= 0; i--)
+    {
+        final += ret[i];
+        if (ret[i] == "error")
+        {
+            store.push_back("error");
+            return;
+        }
+    }
+    store.push_back(final);
 }
